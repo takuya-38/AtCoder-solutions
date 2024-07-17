6aa10c5eef91e4d9dec40bf3a2f8f7369e4eaded
takuya-38 <takuya.kogoma.38@gmail.com>
2024-07-17 22:42:44 +0900

    update: B問題_359

--- past_contests/ABC/B/359.rb
+++ past_contests/ABC/B/359.rb
@@ -12,3 +12,13 @@ array = []
 end
 
 puts count
+
+# n = gets.chomp.to_i
+# a = gets.chomp.split(" ").map { |a| a.to_i }
+# count = 0
+# for i in 0..(a.length - 3) do
+#   if a[i] == a[i + 2]
+#     count += 1
+#   end
+# end
+# puts count.to_s
